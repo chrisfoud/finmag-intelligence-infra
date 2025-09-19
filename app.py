a2460aa2c75b47a49faeddc2c4e7b2fe33aee5dd
chrisfoud <christos.foudoulis@cloudnomads.com.cy>
2025-09-19 11:43:33 +0300

    remove data stack from app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import os
 
 from aws_cdk import App, Environment
 from network_infra.network_stack import NetworkStack
-from s3_rds_elasticache_infra.s3_rds_elasticahce_stack import S3_RDS_Elasticache
+# from s3_rds_elasticache_infra.s3_rds_elasticahce_stack import S3_RDS_Elasticache
 
 app = App()
 
-# Get environment with fallback
+# Get environment
 env = cdk.Environment(
     account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
     region=os.environ.get("CDK_DEFAULT_REGION")
 )
 
 network_stack = NetworkStack(app, "Network-Stack", env=env)
-s3_rds_elasticache = S3_RDS_Elasticache(app, "S3-RDS-Elasticache-Stack", env=env)
+# s3_rds_elasticache = S3_RDS_Elasticache(app, "S3-RDS-Elasticache-Stack", env=env)
 
 
 # Add dependency
